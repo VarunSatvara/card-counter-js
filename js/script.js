d9aa843384015cc43adba3a1bf53e3fd46b5a485
varun satvara <varunsatvara1997@gmail.com>
2020-01-04 00:08:40 +0530

    update

--- js/script.js
+++ js/script.js
@@ -1,40 +1,13 @@
-function add() {
-    var rdiv = document.createElement('div');
-    rdiv.className = "row";
+var count = 0;
 
-    var sdiv = document.createElement('div');
-    sdiv.classList.add("col", "s4")
-    rdiv.appendChild(sdiv);
-
-    var mydiv = document.createElement('div');
-    mydiv.classList.add("card", "larger");
-    mydiv.setAttribute("id", "cardadd")
-    sdiv.appendChild(mydiv);
-
-
-    var mydiv1 = document.createElement('div');
-    mydiv.appendChild(mydiv1);
-    mydiv1.classList.add("card-image", "waves-effect", "waves-block", "waves-light");
-
-    mydiv1.innerHTML = '<img class="activator" src="images/sample-1 (1).jpg">';
-
-    var mydiv2 = document.createElement('div');
-    mydiv2.classList.add("card-content");
-    mydiv2.setAttribute("id", "mydiv");
-    mydiv.appendChild(mydiv2);
-
-    var myp = document.createTextNode('le I am a very simple card. I am good at containing small bits of information. I am convenient because I require little. Although humans are part of nature, human activity is often understood as a separate category from other natural.');
-    mydiv2.appendChild(myp);
-
-    var mydiv3 = document.createElement('div');
-    mydiv3.classList.add("card-action");
-    mydiv.appendChild(mydiv3);
-    mydiv3.innerHTML = '<a class="waves-effect waves-light btn" onclick="remove()">DELETE</a>';
-    document.getElementsByClassName('row')[0].appendChild(sdiv);
-    console.log(rdiv);
+function addcard() {
+    count++;
+    var mydiv = document.getElementById("content");
+    mydiv.insertAdjacentHTML("beforeend", '<div class="col s12 m4" id="item' + count + '"><div class="card" > <div class="card-image"> <img src="images/sample-1 (1).jpg"> <span class="card-title">Card Title</span> </div><div class="card-content"> <p>I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.</p></div><div class="card-action"> <a href="#" onclick="removecard(' + count + ')">Delete</a> </div></div></div>');
+    console.log(mydiv);
 }
 
-function remove() {
-    var elem = document.getElementById("mydiv");
-    elem.parentNode.removeChild(elem);
+function removecard(index) {
+    var getItem = document.getElementById("item" + index);
+    getItem.remove();
 }
