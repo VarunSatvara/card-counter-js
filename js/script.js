f4531b3f9aeb0a7ca3f68570bbaa2c8e336ed7b8
varun satvara <varunsatvara1997@gmail.com>
2020-01-04 00:19:13 +0530

    update

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ var count = 0;
 function addcard() {
     count++;
     var mydiv = document.getElementById("content");
-    mydiv.insertAdjacentHTML("beforeend", '<div class="col s12 m4" id="item' + count + '"><div class="card" > <div class="card-image"> <img src="images/sample-1 (1).jpg"> <span class="card-title">Card Title</span> </div><div class="card-content"> <p>I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.</p></div><div class="card-action"> <a href="#" onclick="removecard(' + count + ')">Delete</a> </div></div></div>');
+    mydiv.insertAdjacentHTML("beforeend", '<div class="col s12 m4" id="item' + count + '"><div class="card" > <div class="card-image"> <img src="images/sample-1 (1).jpg"> <span class="card-title">Card Title</span> </div><div class="card-content"> <p>I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.</p></div><div class="card-action"> <a href="#" onclick="removecard(' + count + ')" class="grey darken-4 btn-remove">Delete' + count + '</a> </div></div></div>');
     console.log(mydiv);
 }
 
